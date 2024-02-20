8a1fb263c37e3769aaea1d14cf3dff294058571a
Shino <shinogasia@gmail.com>
2024-02-19 21:17:06 -0700

    Update metadata for mailing service

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL != null
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase'
+  title: 'Mailing Service',
+  description: 'Envia correos electrónicos a tus clientes de forma sencilla y rápida',
 }
 
 export default function RootLayout ({
