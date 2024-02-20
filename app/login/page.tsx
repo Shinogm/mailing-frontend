323c3cd74e46d036437b1346c6c6e5e54f3622ce
Shino <shinogasia@gmail.com>
2024-02-19 21:07:48 -0700

    Refactor login page layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,6 @@ export default function Login ({
 }) {
   const signIn = async (formData: FormData) => {
     'use server'
-
     const email = formData.get('email') as string
     const password = formData.get('password') as string
     const supabase = createClient()
@@ -51,28 +50,8 @@ export default function Login ({
   }
 
   return (
-    <div className='flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2'>
-      <Link
-        href='/'
-        className='absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm'
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          className='mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1'
-        >
-          <polyline points='15 18 9 12 15 6' />
-        </svg>{' '}
-        Back
-      </Link>
-
+    <div className='flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2'> 
+      
       <form
         className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'
         action={signIn}
@@ -83,7 +62,7 @@ export default function Login ({
         <input
           className='rounded-md px-4 py-2 bg-inherit border mb-6'
           name='email'
-          placeholder='you@example.com'
+          placeholder='correo@example.com'
           required
         />
         <label className='text-md' htmlFor='password'>
